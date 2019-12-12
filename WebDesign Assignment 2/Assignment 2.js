1f9624e68735622ba8ef166717a4bb820aee938b
Alex Woehler <atwbr9@mail.umsl.edu>
2019-12-12 09:05:21 -0600

    yesterdays work

--- WebDesign Assignment 2/Assignment 2.js
+++ WebDesign Assignment 2/Assignment 2.js
@@ -1,5 +1,7 @@
 function getwords() {
     text = words.value;
+    document.getElementById("demo").innerHTML = Date();
     document.getElementById("para").innerHTML += '<p>'+text
     document.getElementById("words").value = "enter"
+    
   }
