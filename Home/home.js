ead6a8e62826a6d42f673fc79d173b12d268c25f
strnfk <47372031+strnfk@users.noreply.github.com>
2019-12-15 00:04:19 -0600

    needs a better looking nav

--- Home/home.js
+++ Home/home.js
@@ -1,32 +1,20 @@
-var $menu = $('.Menu-list'),
-    $item = $('.Menu-list-item'),
-    w = $(window).width(), //window width
-    h = $(window).height(); //window height
+window.onload = () => {
+    if (localStorage.getItem("loggedInProfiles") !== null) {
+      texts = localStorage.getItem("loggedInProfiles");
+      objs = JSON.parse (texts);
 
-$(window).on('mousemove', function(e) {
-  var offsetX = 0.5 - e.pageX / w, //cursor position X
-      offsetY = 0.5 - e.pageY / h, //cursor position Y
-      dy = e.pageY - h / 2, //@h/2 = center of poster
-      dx = e.pageX - w / 2, //@w/2 = center of poster
-      theta = Math.atan2(dy, dx), //angle between cursor and center of poster in RAD
-      angle = theta * 180 / Math.PI - 90, //convert rad in degrees
-      offsetPoster = $menu.data('offset'),
-      transformPoster = 'translate3d(0, ' + -offsetX * offsetPoster + 'px, 0) rotateX(' + (-offsetY * offsetPoster) + 'deg) rotateY(' + (offsetX * (offsetPoster * 2)) + 'deg)'; //poster transform
+      console.log("WINDOW ON LOAD: ", objs[0].loggedIn);
+    }
+  
+  };
 
-  //get angle between 0-360
-  if (angle < 0) {
-    angle = angle + 360;
-  }
+  loginLink = () => {
+    let user = JSON.parse(localStorage.getItem("loggedInProfiles"))
+    console.log("user: ", user);
+    console.log("objs: ", objs[0].loggedIn);
+    
+    if (objs[0].loggedIn==true) {
+        window.location.href = '../Feed/feed.html';
+    } 
+    };
 
-  //poster transform
-  $menu.css('transform', transformPoster);
-
-  //parallax for each layer
-  $item.each(function() {
-    var $this = $(this),
-        offsetLayer = $this.data('offset') || 0,
-        transformLayer = 'translate3d(' + offsetX * offsetLayer + 'px, ' + offsetY * offsetLayer + 'px, 20px)';
-
-    $this.css('transform', transformLayer);
-  });
-});
